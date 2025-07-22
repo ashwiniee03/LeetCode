f735c05db5f768b9df965323730a6c1e4845417e
ashwiniee03 <155455816+ashwiniee03@users.noreply.github.com>
2025-07-23 00:27:58 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (98.45%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -5,11 +5,11 @@ public:
         int l=0;
         int h= n-1;
         int mid;
-        int ans=-1;
+        
         while(l<=h){
             mid= (l+h)/2;
             if(nums[mid]==target){
-                ans=mid;
+                return mid;
                 break;
             }
             else if(nums[mid]<target){
@@ -19,6 +19,6 @@ public:
                 h=mid-1;
             }
         }
-        return ans;
+        return -1;
     }
 };
