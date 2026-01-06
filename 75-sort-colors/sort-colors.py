642160b7b3ecf6ba7719203d2bd165502ca69c2c
ashwiniee03 <155455816+ashwiniee03@users.noreply.github.com>
2026-01-06 22:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (13.46%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,5 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        count_dict= {0:0, 1:0, 2:0}
+        n= len(nums)
+        for i in nums:
+            count_dict[i]= count_dict[i]+1
+        
+        j=0
+        
+        for i in count_dict:
+            for k in range (count_dict[i]):
+                nums[j]= i
+                j+=1
+
+        return nums
+
+
+
         
