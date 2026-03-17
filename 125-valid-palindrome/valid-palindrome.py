095870f022af33a4b4dedeffc490a222a9bc55c7
ashwiniee03 <155455816+ashwiniee03@users.noreply.github.com>
2026-03-17 23:37:58 +0530

    Time: 148 ms (18.68%) | Memory: 13 MB (60.36%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,21 +1,25 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = "".join(ch.lower() for ch in s if ch.isalnum())
-        def helper(start:int, end:int):
-            if (start>=end):
-                return True
-            if (s[start]!= s[end]):
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        st=''
+        for i in s:
+            if (i.isalnum()):
+                st+=i.lower()
+            
+        n= len (st)
+        i=0
+        j= n-1
+
+        while(i<=j):
+            if (st[i]== st[j]):
+                i+=1
+                j-=1
+            else:
                 return False
 
-            return helper(start+1, end-1)
-
-        return helper(0, len(s)-1)
-        def helper(start:int, end:int):
-            if (start>=end):
-                return True
-            if (s[start]!= s[end]):
-                return False
-
-            return helper(start+1, end-1)
-
-        return helper(0, len(s)-1)
+        return True
+        
