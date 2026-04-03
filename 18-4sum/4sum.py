9568ec9e2f712fedc6b08d44940f440378a4456b
ashwiniee03 <155455816+ashwiniee03@users.noreply.github.com>
2026-04-03 16:22:26 +0530

    Time: 535 ms (47.44%) | Memory: 12.2 MB (96.75%) - LeetSync

---
+++ 18-4sum/4sum.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+
+        result=[]
+        n= len (nums)
+        nums.sort()
+        
+        for i in range(n):
+            if i>0 and nums[i]== nums[i-1]:
+                continue
+            for j in range(i+1,  n):
+                if j>i+1 and nums[j]== nums[j-1]:
+                    continue
+                k,l= j+1, n-1
+
+                while(k<l):
+                    total= nums[i]+nums[j]+nums[k]+nums[l]
+                    if (total == target):
+                        result.append([nums[i],nums[j],nums[k],nums[l]])
+                    
+                        k+=1
+                        l-=1
+                        while  k<l and nums[k]== nums[k-1]:
+                            k+=1
+
+                        while  k<l and nums[l]== nums[l+1]:
+                            l-=1
+
+                    elif total> target:
+                        l-=1
+
+                    else:
+                        k+=1
+
+        return result
+        
+                    
+        
